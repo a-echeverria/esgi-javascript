150c9237e7ead74d631caddf47157b43b339fd9f
Echeverria Andrea <echeverriaandrea20@gmail.com>
2020-10-23 18:01:13 +0200

    type check v1 && v2

---
+++ exercise-2/type-check.js
@@ -0,0 +1,24 @@
+function type_check_v1(value, type) {
+    if (typeof value === "object") {
+        if (type === "array") {
+            return Array.isArray(value);
+        }
+        if (type === "null") {
+            return value === null;
+        }
+    }
+    return typeof value === type;
+}
+
+function type_check_v2(value, type) {
+    if (type["type"] && typeof value !== type["type"]) {
+        return false;
+    }
+    if (type["value"] && value !== type["value"]){
+        return false
+    }
+    if (type["enum"] && !(type["enum"].includes(value))) {
+        return false;
+    }
+    return true;
+}
